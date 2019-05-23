9aeb07ea86d2a33bae269044b4f525c41ff64ecb
Fma <fmakdemir@users.noreply.github.com>
2019-05-23 15:48:57 +0300

    added outline and mock code for neighbor walk kernel

---
+++ kernels/neighbor_walk_kernel.py
@@ -0,0 +1,11 @@
+New Kernel:
+for pw in all_pathways:
+    for n in pw:
+        rw = random walks x RW_CNT
+        vis_freq_map = freq_count_of_genes(rw)
+        neigh_w[n] = calculate_weigted_neighbor_list(vis_freq_map)
+
+for p in patients:
+    for m in mutate_genes[p]:
+        for n in neighbors[m]:
+            w[n] += neigh_w[m][n]
