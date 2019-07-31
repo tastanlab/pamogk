e39ea6b1dedeb109881e95cc7402f16612b40ac2
Fma <fmakdemir@users.noreply.github.com>
2019-07-31 18:18:43 +0300

    added kernel converter for testing rmkkml

---
+++ lib/kernel_converter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import csv
+import numpy as np
+import sys
+import scipy.io as sio
+import traceback
+
+args = sys.argv[1:]
+
+for p in args:
+    try:
+        print('Converting kernel matrices from file:', p)
+        kms = np.load(p)['kms']
+        s = kms.shape
+        i_lower = np.tril_indices(s[1], -1)
+        for i, m in enumerate(kms):
+            m[i_lower] = m.T[i_lower] # for precision errors mirror upper half
+            print('  Writing kernel file {:3}/{}'.format(i+1, s[0]), end='\r')
+            with open('/tmp/kernel-{}.csv'.format(i), 'w') as f:
+                sio.savemat('/tmp/kms-{}'.format(i), { 'out_kernel{}_'.format(i): m })
+                c = csv.writer(f)
+                for r in m: c.writerow(r)
+        sio.savemat('/tmp/kms.mat', { 'kms': kms})
+        print('\nFinished')
+    except:
+        print('Failed to convert kernel matrices from given path:', p)
+        traceback.print_exc()
+
