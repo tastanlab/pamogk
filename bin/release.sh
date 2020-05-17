b59d289602bc2f7a24920356af81db19bc62c58e
Fma <fmakdemir@users.noreply.github.com>
2020-05-17 18:33:16 +0300

    feat: added prod deploy mode

--- bin/release.sh
+++ bin/release.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 pipenv run python setup.py sdist bdist_wheel
-pipenv run twine upload --repository testpypi dist/*
+
+repo=testpypi
+if [[ "$1" -eq "prod" ]]; then
+	repo=pypi
+fi
+pipenv run twine upload --repository $repo dist/*
